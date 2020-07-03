c2e8593a907145f8ddd9da1c7172907d0a392a77
Alexander Protto <alexander.protto@realpage.com>
2020-07-03 12:36:08 -0500

    Added DataAccess library class

---
+++ CaroOverview/Helpers/DataProcessor.cs
@@ -0,0 +1,48 @@
+﻿using CaroOverview.Model;
+using DataLibrary.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaroOverview.Helpers
+{
+    public static class DataProcessor
+    {
+        public static List<Rmledg> GetRmledgs() 
+        {
+            string query = @"SELECT 
+                                [id],
+                                IsActive
+                                FROM [asbstaging].[mri].[Rmledg]
+                                WHERE [IsActive] = 'Y';";
+
+            return SqlDataAccess.LoadData<Rmledg>(query);
+                
+        }
+
+        public static int SaveRmledgRecord(Rmledg rmledgObject)
+        {
+            string query = "Uo";
+
+            //Rmledg rmledgData = new Rmledg();
+
+            //rmledgData.CHGCODE = "dksjhfkjsd";
+
+
+            //SaveData<T>(string query, T data)
+            return SqlDataAccess.SaveData(query, rmledgObject);
+        }
+
+        public static int UpdateRmledgRecord(Rmledg rmledgObject, int id)
+        {
+            string query = "Uo";
+
+
+            //SaveData<T>(string query, T data)
+            return SqlDataAccess.SaveData(query, rmledgObject);
+        }
+
+    }
+}
---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace DataLibrary.DataAccess
+{
+    public static class SqlDataAccess
+    {
+        private static readonly string _connectionName = "Server=ServerName;initial catalog=DataBaseName;persist security info=True;user id=UserNameForDatabase;password=passwordIsHere;MultipleActiveResultSets=True;";
+
+        public static int SaveData<T>(string query, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(_connectionName))
+            {
+                return cnn.Execute(query, data);
+            }
+
+        }
+
+        public static List<T> LoadData<T>(string query)
+        {
+            using (IDbConnection cnn = new SqlConnection(_connectionName))
+            {
+                return cnn.Query<T>(query).ToList();
+            }
+        }
+
+
+    }
+
+
+}
